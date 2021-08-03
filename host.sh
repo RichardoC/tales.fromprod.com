df26001bf0e0fa4fcdc71cd472c39ee301cade15
Richard Finlay Tweed <richardf@RFT-Mac.lan>
2021-08-03 21:29:46 +0100

    Starting to add a Jekyll site

---
+++ host.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source JEKYLL_VERSION.sh
+
+docker run --rm --publish [::1]:4000:4000 --volume="$PWD:/srv/jekyll" --volume="$PWD/.gemdata:/usr/local/bundle" -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve -s /srv/jekyll/jekyll -d /srv/jekyll/docs
---
+++ prep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source JEKYLL_VERSION.sh
+
+docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/.gemdata:/usr/local/bundle" -it jekyll/jekyll:$JEKYLL_VERSION jekyll build -s /srv/jekyll/jekyll -d /srv/jekyll/docs
