8fe07328bee6234cc4396378bcfdcc92881b8b52
Richard Tweed <RichardoC@users.noreply.github.com>
2021-11-24 18:10:31 +0000

    Slack/Discord Link preview article (#5)
    * Attempting to make lima work for host/generate
    
    * Includes boilerplate for pi writeup

--- generate.sh
+++ generate.sh
@@ -2,4 +2,16 @@
 
 source JEKYLL_VERSION.sh
 
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/.gemdata:/usr/local/bundle" -it jekyll/jekyll:$JEKYLL_VERSION jekyll build -s /srv/jekyll/src -d /srv/jekyll/docs
+
+export _BINARY="docker"
+
+if [ -x "$(command -v lima)" ]; then
+  echo 'lima is installed so using it instead' >&2
+  _BINARY="lima nerdctl "
+elif [ -x "$(command -v nerdctl)" ]; then
+  echo 'nerdctl is installed so using it instead' >&2
+  _BINARY="nerdctl "
+fi
+
+
+$_BINARY run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION jekyll build -s /srv/jekyll/src -d /srv/jekyll/docs
--- host.sh
+++ host.sh
@@ -4,4 +4,15 @@ source JEKYLL_VERSION.sh
 
 ./clean.sh
 
-docker run --rm --publish 127.0.0.1:4000:4000 --volume="$PWD:/srv/jekyll" --volume="$PWD/.gemdata:/usr/local/bundle" -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve -s /srv/jekyll/src -d /srv/jekyll/docs-tmp --watch --incremental
+export _BINARY="docker"
+
+if [ -x "$(command -v lima)" ]; then
+  echo 'lima is installed so using it instead' >&2
+  _BINARY="lima nerdctl "
+elif [ -x "$(command -v nerdctl)" ]; then
+  echo 'nerdctl is installed so using it instead' >&2
+  _BINARY="nerdctl "
+fi
+
+
+$_BINARY run --rm --p 4000:4000 --volume="$(pwd):/srv/jekyll:rw" -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve -s /srv/jekyll/src -d /srv/jekyll/docs-tmp --watch --incremental
