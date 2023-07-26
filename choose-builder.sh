7671fab9b6caedb70110dfa7381ac8d4cb4e7a17
Richard Tweed <RichardoC@users.noreply.github.com>
2023-07-26 23:24:46 +0100

    Refactor logic for choosing builder, and bump deps

---
+++ choose-builder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source JEKYLL_VERSION.sh
+
+
+export _BINARY="docker"
+
+# if [ -x "$(command -v lima)" ]; then
+#   echo 'lima is installed so using it instead' >&2
+#   _BINARY="lima nerdctl "
+# elif [ -x "$(command -v nerdctl)" ]; then
+#   echo 'nerdctl is installed so using it instead' >&2
+#   _BINARY="nerdctl "
+# fi
