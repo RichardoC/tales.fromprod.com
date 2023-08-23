0bd2e5cf607b3d4a0276c824699d6fd0f3a8e8a1
Richard Tweed <RichardoC@users.noreply.github.com>
2023-08-23 17:53:12 +0100

    Update gems, fix scripts using choose-builder.sh and regenerated the site (#15)

--- host.sh
+++ host.sh
@@ -4,5 +4,4 @@ source JEKYLL_VERSION.sh
 
 source ./choose-builder.sh
 
-
 $_BINARY run -p 4000:4000 --rm --volume="$(pwd):/srv/jekyll:rw" --volume="jekyll:/usr/local/bundle" -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve -s /srv/jekyll/src -d /srv/jekyll/docs-tmp --watch --incremental
