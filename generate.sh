531c3b5a26320478a1bae754bae2fca06ce54a2c
Richard Tweed <RichardoC@users.noreply.github.com>
2024-09-03 17:11:16 +0100

    Test fix for github actions

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ source ./choose-builder.sh
 
 $_BINARY build -t jekyll .
 
-$_BINARY run --rm --volume="$PWD:/srv/jekyll:rw" --user root -it jekyll:latest build -s /srv/jekyll/src -d /srv/jekyll/docs
+$_BINARY run --rm --volume="$PWD:/srv/jekyll:rw" --user root  jekyll:latest build -s /srv/jekyll/src -d /srv/jekyll/docs
