e20bbb4dc4089e3c937964a76d289102d0f72c75
Richard Tweed <RichardoC@users.noreply.github.com>
2024-09-22 19:13:57 +0100

    Fail to record audio with a tildagon badge

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@
 
 source ./choose-builder.sh
 
-$_BINARY build -t jekyll .
+$_BINARY build -t my-jekyll .
 
-$_BINARY run --rm --volume="$PWD:/srv/jekyll:rw" --user root  jekyll:latest build -s /srv/jekyll/src -d /srv/jekyll/docs
+$_BINARY run --rm --volume="$PWD:/srv/jekyll:rw" --user root  my-jekyll:latest build -s /srv/jekyll/src -d /srv/jekyll/docs
--- host.sh
+++ host.sh
@@ -2,4 +2,4 @@
 
 source ./choose-builder.sh
 
-$_BINARY run -p 4000:4000 --rm --volume="$(pwd):/srv/jekyll:rw" -it jekyll serve -s /srv/jekyll/src -d /srv/jekyll/docs-tmp --watch --incremental -H 0.0.0.0
+$_BINARY run -p 4000:4000 --rm --volume="$(pwd):/srv/jekyll:rw" -it my-jekyll:latest serve -s /srv/jekyll/src -d /srv/jekyll/docs-tmp --watch --incremental -H 0.0.0.0
