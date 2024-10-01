fd5dd8c2c5cd110d7d95297c140233431905f21a
Richard Tweed <RichardoC@users.noreply.github.com>
2024-10-01 14:07:24 +0100

    create image and fix updater

--- update-gemfile.sh
+++ update-gemfile.sh
@@ -2,4 +2,6 @@
 
 source ./choose-builder.sh
 
-$_BINARY run -p 4000:4000 --rm  --user root -it --entrypoint bundle --volume="$(pwd):/srv/jekyll:rw" -it my-jekyll:latest update --all
+$_BINARY build -t my-jekyll:updates . -f Dockerfile-updates
+
+$_BINARY run -p 4000:4000 --rm  --user root -it --entrypoint bundle --volume="$(pwd):/srv/jekyll:rw" -it my-jekyll:updates update --all
